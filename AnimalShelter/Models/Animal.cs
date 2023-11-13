13f8e75defc0667ac39d345379bb5793bc2416e9
Aidan Williams <aidanwilliams01@gmail.com>
2023-11-13 14:33:57 -0800

    add initial UI logic

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
 
 namespace AnimalShelter.Models
 {
   public class Animal
   {
     public string Name { get; set; }
-    public string AdmittanceDate { get; set; }
+    public DateTime AdmittanceDate { get; set; }
     public string Breed { get; set; }
-    public int Type { get; set; }
+    public string Type { get; set; }
     public int AnimalId { get; set; }
   
-    public Animal(string name, string admittanceDate, string breed, int type, int id)
+    public Animal(string name, DateTime admittanceDate, string breed, string type, int id)
     {
       Name = name;
       AdmittanceDate = admittanceDate;
@@ -20,5 +22,7 @@ namespace AnimalShelter.Models
       Type = type;
       AnimalId = id;
     }
+
+    public Animal() {}
   }
 }
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -4,7 +4,6 @@ namespace AnimalShelter.Models
 {
   public class AnimalShelterContext : DbContext
   {
-    public DbSet<Type> Types { get; set; }
     public DbSet<Animal> Animals { get; set; }
 
     public AnimalShelterContext(DbContextOptions options) : base(options) { }
